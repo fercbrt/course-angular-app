37a7c33f7c2afbd577d42581de1f3c255248e54b
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-14 11:58:43 +0200

    Completed third lesson

---
+++ src/app/products/services/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Angular',
+      price: 100,
+      description: 'Angular Course'
+    },
+    {
+      id: 2,
+      name: 'Spring Boot',
+      price: 150,
+      description: 'Spring Boot Course'
+    },
+    {
+      id: 3,
+      name: 'MySQL',
+      price: 200,
+      description: 'MySQL Course'
+    }
+  ];
+  constructor() { }
+}
