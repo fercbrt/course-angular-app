1d1d9b5f15334d0b667585f8b82592abe8b3c2eb
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-14 18:03:24 +0200

    Completed the sixth lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
 import { ProductComponent } from './products/components/product/product.component';
+import { FormComponent } from './products/components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductComponent],
+  imports: [CommonModule, RouterOutlet, ProductComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/products/components/form/form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  product: Product = {
+    id: 0,
+    name: 'Keyboard',
+    price: 800,
+    description: 'It\'s a keyboard'
+  };
+}
